5cc5fdbfdc260ac92d8c9f928d2dc10b4863f7bc
Nataliia <natashazaremba82@gmail.com>
2025-08-03 19:30:47 +0300

    change6

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,11 +1,24 @@
 import NoteDetails from "./NoteDetails";
+import { fetchNoteById } from "@/lib/api";
+import type { Metadata } from "next";
 
-interface NotePageProps {
+type GenerateMetadataProps = {
   params: {
     id: string;
   };
+};
+
+export async function generateMetadata({
+  params,
+}: GenerateMetadataProps): Promise<Metadata> {
+  const note = await fetchNoteById(String(params.id));
+
+  return {
+    title: note?.title || "Note Not Found",
+    description: note?.content?.slice(0, 100) || "No content available",
+  };
 }
 
-export default function NotePage({ params }: NotePageProps) {
+export default function Page() {
   return <NoteDetails />;
 }
